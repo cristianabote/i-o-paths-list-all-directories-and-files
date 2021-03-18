4d683373fc54014ac93bf83c7c4c132759245a6a
cristianabote <cristianamihaelaplesa@gmail.com>
2021-03-18 22:11:11 +0200

    another commit

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -7,9 +7,23 @@ import java.util.stream.Collectors;
 
 public class Application {
     public static void main(String[] args) throws IOException {
-        Path specificPath= Paths.get("C:", "Java", "Proiecte");
+        /*
+I/O - Paths - List all directories and files
+List all files and directories from a specific path.
+
+Input (example)
+/users/andrei/IdeaProjects
+
+Output
+exception-handling
+.IntelliJ
+notes.txt
+ */
+        Path specificPath= Paths.get("C:", "JAVA-modul 1", "kit");
+        //System.out.println( specificPath+" is directory? "+ Files.isDirectory(specificPath));
+        //System.out.println(specificPath+" is regular file? "+Files.isRegularFile(specificPath));
         List<Path> allDirAndFiles=Files.list(specificPath).collect(Collectors.toList());
-        System.out.println("Content of "+specificPath+" :");
+        System.out.println("Content of "+specificPath+":");
         for(Path p: allDirAndFiles) {
             System.out.println("- "+ p.getFileName() + "; ");
 
@@ -17,19 +31,3 @@ public class Application {
     }
 }
 
-/*
-I/O - Paths - List all directories and files
-List all files and directories from a specific path.
-
-Input (example)
-
-/users/andrei/IdeaProjects
-
-Output
-
-exception-handling
-
-.IntelliJ
-
-notes.txt
- */
